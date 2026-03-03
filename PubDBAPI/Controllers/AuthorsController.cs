e56dd1c8c4daf003a4083ec921d4aa888da93d20
Mesihovic <mersiha.mesihovic.webd25jon@edu.tucsweden.se>
2026-03-03 15:32:23 +0100

    feat: improve AuthorsController with Include and AsNoTracking

--- PubDBAPI/Controllers/AuthorsController.cs
+++ PubDBAPI/Controllers/AuthorsController.cs
@@ -21,33 +21,35 @@ namespace PubDBAPI.Controllers
             _context = context;
         }
 
-        // GET: api/Authors
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
-        {
-            return await _context.Authors.ToListAsync();
-        }
+    // GET: api/Authors
+    [HttpGet]
+     public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+    {
+    return await _context.Authors
+        .Include(a => a.Books)
+        .AsNoTracking()
+        .ToListAsync();
+     }
 
-        // GET: api/Authors/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Author>> GetAuthor(int id)
-        {
-            var author = await _context.Authors.FindAsync(id);
+     // GET: api/Authors/5
+     [HttpGet("{id}")]
+     public async Task<ActionResult<Author>> GetAuthor(int id)
+    {
+    var author = await _context.Authors
+        .Include(a => a.Books)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(a => a.AuthorId == id);
 
-            if (author == null)
-            {
-                return NotFound();
-            }
-
-            return author;
-        }
+     if (author == null) return NotFound();
+     return author;
+}
 
         // PUT: api/Authors/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutAuthor(int id, Author author)
         {
-            if (id != author.AuthorId)
+            if (id != author.AuthorId) 
             {
                 return BadRequest();
             }
